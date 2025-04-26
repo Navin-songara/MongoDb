ce18340e45ab9ed083978e61afd027731d7bb585
Navin Songara <navinsongara1@gmail.com>
2025-04-26 23:32:15 +0530

    Add files via upload
    q

---
+++ book.model.js
@@ -0,0 +1,9 @@
+// book model file is used to define schema of database , here we can define field or struture  of table or collection
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var Book = new Schema({
+    BId: { type: Number },
+    BName: { type: String },
+    BPrice: { type: Number },
+}, { collection: 'Book' });
+module.exports = mongoose.model('Book', Book);
