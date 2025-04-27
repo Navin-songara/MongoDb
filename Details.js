c4773c544a99d4ff708a4d0bf25b7de1e05d4394
Navin Songara <navinsongara1@gmail.com>
2025-04-27 22:37:43 +0530

    Add files via upload
    a

---
+++ Details.js
@@ -0,0 +1,15 @@
+// props is used to pass the data from one component to next component. 
+//react-dom/client named library provides function to call a component from one component.
+
+import React from "react";
+
+function Details(props)
+{
+    return(
+        <div>
+            <center>
+                <h4>Welcome {props.ssname}</h4>
+            </center>
+        </div>
+    );
+}export default Details;
