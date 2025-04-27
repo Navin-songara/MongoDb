1f60c077f282806e7d9add36c4809de86a652d44
Navin Songara <navinsongara1@gmail.com>
2025-04-27 22:45:59 +0530

    Add files via upload
    j

---
+++ ArmstrongComponent.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function ArmstrongComponent() {
+    const [num, setNum] = useState('');
+    const [res, setRes] = useState('');
+
+    const handleNumText = (evt) => {
+        setNum(evt.target.value);
+    }
+
+    const handleCheckButton = () => {
+        const n = parseInt(num);
+        const digits = n.toString().split('');
+        const power = digits.length;
+        const sum = digits.reduce((acc, digit) => acc + Math.pow(parseInt(digit), power), 0);
+
+        if (sum === n) {
+            setRes("Number is Armstrong");
+        } else {
+            setRes("Number is not Armstrong");
+        }
+    }
+
+    return (
+        <div>
+            <center>
+                <h4>Check Armstrong Number</h4>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Enter Number</td>
+                            <td>
+                                <input type="number" onChange={handleNumText} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Result</td>
+                            <td>
+                                <input type="text" value={res} readOnly />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2" style={{ textAlign: "center" }}>
+                                <button type="submit" onClick={handleCheckButton}>Check</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </center>
+        </div>
+    );
+}
+
+export default ArmstrongComponent;
