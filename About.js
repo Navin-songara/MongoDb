db019ab94fa086f76ad5c7f8cd75d16273899029
Navin Songara <navinsongara1@gmail.com>
2025-05-05 23:56:54 +0530

    Add files via upload
    adf

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About()
+{
+    return(
+        <div>
+            <div>About Us Page</div>
+            <center>
+                <h4>Home Page</h4>
+                <p>Universal Informatics IT Training Center Indore</p>
+                <p>Sabse sasta sabse.........samjh gaye</p>
+            </center>
+        </div>
+    );
+}export default About;
