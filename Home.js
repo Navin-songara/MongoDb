1581ae17aa4a89d003883d7f3c0118a8735bab6b
Navin Songara <navinsongara1@gmail.com>
2025-05-05 23:57:59 +0530

    Add files via upload
    zv

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home()
+{
+    return(
+        <div>
+            <div>React Routing Program</div>
+            <center>
+                <h4>Home Page</h4>
+                <p>welcome this is my home page</p>
+                <p>Universal Informatics Indore</p>
+            </center>
+        </div>
+    );
+}export default Home;
