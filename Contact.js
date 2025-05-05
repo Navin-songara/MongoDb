c538aaaf98137a317ba60a0a065bf69826f726a8
Navin Songara <navinsongara1@gmail.com>
2025-05-05 23:57:14 +0530

    Add files via upload
    cdz

---
+++ Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Contact()
+{
+    return(
+        <div>
+            <center>
+                <h4>welcome Contact Us Page</h4>
+                <p>
+                    Contact Details:-
+                </p>
+                <p>Address:- Near Bholaram gate Vaishoupuri A.B. Road Indore</p>
+                <p>Emails:-emailuniversal.com</p>
+                <p>Contact No:- 9376345671</p>
+            </center>
+        </div>
+    );
+}export default Contact;
