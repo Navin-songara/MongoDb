6c14af24219a5a403e5998c03d8ca81ef7b89b07
Navin Songara <navinsongara1@gmail.com>
2025-06-18 22:37:21 +0530

    Update book.model.js
    i added comment for better understanding

--- book.model.js
+++ book.model.js
@@ -1,5 +1,6 @@
 // book model file is used to define schema of database , here we can define field or struture  of table or collection
 var mongoose = require("mongoose");
+// mongoose
 var Schema = mongoose.Schema;
 var Book = new Schema({
     BId: { type: Number },
